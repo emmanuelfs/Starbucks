3575ab1fb82920d7d0ed4bfbe93c93e2563f7735
emmanuelfs <efsantinoli@gmail.com>
2022-03-02 23:34:03 -0300

    Clean up

--- javaScript.js
+++ javaScript.js
@@ -5,13 +5,18 @@ function load()
     clickFooterArrow.addEventListener("click", function() {displayContent("aboutUsContainer","arrowAboutUs")}, false);
     clickFooterArrow = document.getElementById("arrowCareers");
     clickFooterArrow.addEventListener("click", function() {displayContent("careersContainer","arrowCareers")}, false);
+    clickFooterArrow = document.getElementById("arrowSocialImpact");
+    clickFooterArrow.addEventListener("click", function() {displayContent("socialImpactContainer","arrowSocialImpact")}, false);
+    clickFooterArrow = document.getElementById("arrowForBusinessPartners");
+    clickFooterArrow.addEventListener("click", function() {displayContent("forBusinessPartnersContainer","arrowForBusinessPartners")}, false);
+    clickFooterArrow = document.getElementById("arrowOrderAndPickup");
+    clickFooterArrow.addEventListener("click", function() {displayContent("orderAndPickupContainer","arrowOrderAndPickup")}, false);
 }
 document.addEventListener("DOMContentLoaded", load, false);
 
 function displayContent(currentlyHiddenValue, spinArrowValue){
   let currentlyHidden = document.getElementById(currentlyHiddenValue);
   let spinArrow = document.getElementById(spinArrowValue);
-  console.log(currentlyHidden.style.display);
   if (currentlyHidden.style.display === "none") {   
       spinArrow.style.transform = "rotate(-180deg)";
       currentlyHidden.style.display = "block";
